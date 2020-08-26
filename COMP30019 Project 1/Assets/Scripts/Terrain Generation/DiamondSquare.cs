5e130ee7aca79f0edb95f809d9f2051cc77e19b1
nate3223 <nathanice3223@gmail.com>
2020-08-27 00:44:38 +1000

    Restructured Diamond Square algorithm

--- COMP30019 Project 1/Assets/Scripts/Terrain Generation/DiamondSquare.cs
+++ COMP30019 Project 1/Assets/Scripts/Terrain Generation/DiamondSquare.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
@@ -6,27 +7,45 @@ using System.Security.Cryptography;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class DiamondSquare : MonoBehaviour
 {
     [Range(2, 100)] public int nValue = 2;
     [Range(0.0f, 1000.0f)] public float baseVariance = 1.0f;
+    [Range(0.1f, 20.0f)] public float maxHeight = 1.0f;
+    [Range(1.0f, 100.0f)] public float spread = 50.0f;
     private float variance = 1.0f;
     private float[][] grid;
     private int gridSize;
+    private Mesh mesh;
+    MeshFilter meshFilter;
     //This function run once when Unity is in Play
     void Start()
     {
         Debug.Log("hello");
+        grid = new[]
+        {
+            new[]{1.0f,0,2.0f,0,3.0f },
+			new[]{4.0f, 0, 5.0f, 0, 6.0f },
+			new[]{7.0f, 0, 8.0f, 0, 9.0f },
+			new[]{10.0f, 0, 11.0f, 0, 12.0f },
+			new[]{13.0f, 0, 14.0f, 0, 15.0f }
+            };
+
+        
         GenerateMesh();
     }
     void GenerateMesh()
 	{
         GenerateEmptyGrid();
+        GenerateEmptyMesh();
         DiamondSquareAlgorithm();
         RemoveDebug();
-
-	}
+        mesh.vertices = CrunchGrid();
+        GetComponent<MeshFilter>().mesh = mesh;
+        GenTriangles();
+    }
 
     void DiamondSquareAlgorithm()
 	{
@@ -37,10 +56,12 @@ public class DiamondSquare : MonoBehaviour
          * Each square is passed throught the Step method, which will then set the designated height values
          */
         //  Set the corners
+        
         SetHeight(new Vector2(0, 0), RandomHeight());
         SetHeight(new Vector2(gridSize - 1, 0), RandomHeight());
         SetHeight(new Vector2(gridSize - 1, gridSize - 1), RandomHeight());
         SetHeight(new Vector2(0, gridSize - 1), RandomHeight());
+        LowerHeight();
 
         variance = baseVariance;
         float maxV = gridSize - 1;
@@ -51,23 +72,78 @@ public class DiamondSquare : MonoBehaviour
             //  Run for each row of squares
             for (int j = 0; j < sqrtSquares; j++)
             {
-                float y = j * divided;
+                float y = j * (divided);
                 //  Run for each column of squares
                 for (int k = 0; k < sqrtSquares; k++)
                 {
-                    float x = k * divided;
+                    float x = k * (divided);
                     Vector2 p1 = new Vector2(x, y);
-                    Vector2 p3 = new Vector2(x, y) + Vector2.one * divided;
+                    Vector2 p3 = new Vector2(x, y) + Vector2.one * (divided);
                     Step(p1, p3);
                 }
             }
-            LowerVariance();
+            LowerHeight();
         }
     }
 
-    void GenerateTriangles()
+    void GenerateEmptyMesh()
 	{
+        mesh = new Mesh();
+	}
+    void GenTriangles()
+    {
+        var triangles = new int[mesh.vertices.Length * 6];
+        int idx = 0;
+        Vector2[] uvs = new Vector2[(int)Math.Pow(gridSize, 2)];
+        for (int z = 0; z <= gridSize-1; z++)
+        {
+            for (int x = 0; x <= gridSize-1; x++)
+            {
+                uvs[z * (gridSize) + x] = new Vector2((float)x / (gridSize - 1), (float)z / (gridSize - 1));
+                if (z < (gridSize-1) && x < (gridSize-1))
+                {
+                    //Triangle 1
+                    //
+                    //|\
+                    //| \
+                    //|__\
+                    triangles[idx + 2] = (z + 1) * (gridSize) + x;
+                    triangles[idx] = (z * (gridSize)) + x + 1;
+                    triangles[idx + 1] = (z * (gridSize)) + x;
+                    //Triangle 2
+                    // __
+                    //\  |
+                    // \ |
+                    //  \|
+                    triangles[idx + 3] = (z + 1) * (gridSize) + x;
+                    triangles[idx + 4] = ((z + 1) * (gridSize)) + x + 1;
+                    triangles[idx + 5] = (z * (gridSize)) + x + 1;
+                    idx += 6;
+                }
+            }
+        }
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
 
+    Vector3[] CrunchGrid()
+	{
+        //  Takes the grid, adds a new vertex corresponding to the height, returns it as a 1D array
+        List<Vector3> crunched = new List<Vector3>();
+        int j = 0;
+        foreach (float[] vs in grid)
+		{
+            int i = 0;
+            foreach (float h in vs)
+			{
+                crunched.Add(new Vector3(i, 0, j) * spread + new Vector3(0, h, 0));
+                i++;
+			}
+            j++;
+		}
+        return crunched.ToArray();
 	}
 
     void Step(Vector2 p1, Vector2 p3)
@@ -91,7 +167,8 @@ public class DiamondSquare : MonoBehaviour
          */
 
         Vector2 mp = (p1 + p3) / 2;
-        SetHeight(mp, GenerateAverage(mp, p1) + RandomHeight());
+        float temp = GenerateAverage(p1, mp);
+        SetHeight(mp, temp + RandomHeight());
 
         /*  Square step
         *
@@ -120,7 +197,8 @@ public class DiamondSquare : MonoBehaviour
         
         foreach (Vector2 v in diamondCorners)
 		{
-            SetHeight(v, GenerateAverage(mp, v) + RandomHeight());
+            temp = GenerateAverage(mp, v);
+            SetHeight(v, temp + RandomHeight());
 		}
     }
 
@@ -180,12 +258,12 @@ public class DiamondSquare : MonoBehaviour
     float RandomHeight()
 	{
         //  Returns a random height based off of the variance
-        return Random.Range(-1.0f, 1.0f) * variance;
+        return Random.Range(-maxHeight, maxHeight);
 	}
-    void LowerVariance()
+    void LowerHeight()
 	{
-        //  Lowers the variance
-        variance = variance * 0.50f;
+        //  Lowers the Height
+        maxHeight = maxHeight * 0.6f;
 	}
     void GenerateEmptyGrid()
 	{
