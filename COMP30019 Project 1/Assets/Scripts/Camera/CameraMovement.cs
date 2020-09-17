20c520b20ead1a20ea48843bb4a06b396ed5dd1c
Timmy Truong <timmy.truong@student.unimelb.edu.au>
2020-09-17 19:16:19 +1000

    Added bounds, aligned world to zero, added text.

--- COMP30019 Project 1/Assets/Scripts/Camera/CameraMovement.cs
+++ COMP30019 Project 1/Assets/Scripts/Camera/CameraMovement.cs
@@ -6,17 +6,21 @@ using Vector3 = UnityEngine.Vector3;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float speed = 10.0f;
-	public float sprintMultiplier = 5.0f;
+    public float speed = 12.5f;
+	public float sprintMultiplier = 1.5f;
 	public Transform player;
 	public new Camera camera;
 	public LayerMask layerMask;
+	public GameObject terrainGenerator;
+	private DiamondSquareV2 dq;
+	private float gridsize;
 	public bool test;
 	public float collisionMultiplier;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dq = terrainGenerator.GetComponent<DiamondSquareV2>();
+		gridsize = dq.GetGridSize();
     }
 
     // Update is called once per frame
@@ -50,7 +54,7 @@ public class CameraMovement : MonoBehaviour
 		float i = 0.001f;
 
 		//	Check if the player will collide with the terrain by 'spawning' a sphere at the next position
-		while(Physics.CheckSphere((player.position + dPosition), 0.5f))
+		while(Physics.CheckSphere((player.position + dPosition), 0.55f))
 		{
 			if (dPosition.normalized == new Vector3 (0, -1, 0))
 			{
@@ -59,6 +63,14 @@ public class CameraMovement : MonoBehaviour
 			}
 			dPosition.y += i;
 			Mathf.Max(0.00001f, i/2f);
+		}
+		if ((player.position + dPosition).x > gridsize - 0.5f || (player.position + dPosition).x < 0.5f)
+		{
+			dPosition.x = 0;
+		}
+		if ((player.position + dPosition).z > gridsize - 0.5f || (player.position + dPosition).z < 0.5f)
+		{
+			dPosition.z = 0;
 		}
 		player.position += dPosition;
     }
